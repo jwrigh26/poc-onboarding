cce12b408e7a2d5cc11f6efa1fb45832938dbbf7
jwrigh26 <jwrigh26@gmail.com>
2023-08-19 10:22:52 -0600

    First successful build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,60 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react-hooks/exhaustive-deps": "off", // Devs can figure out what to pass to useEffect.
+    // NOT SURE NEEDED
+    // Allow us to never have to import React
+    // 'react/jsx-uses-react': 'off',
+    // 'react/react-in-jsx-scope': 'off',
+    // --------- OTHER RULES ---------
+    // show as warnings
+    "prefer-template": "warn",
+    "object-shorthand": "warn",
+    "react/jsx-boolean-value": "warn",
+    "react/jsx-curly-brace-presence": "warn",
+    "react/no-array-index-key": "warn",
+    "prefer-destructuring": "warn",
+    "react/destructuring-assignment": "warn",
+    "no-param-reassign": "off", // TODO: turn this to "warn" after figuring out how to exclude slice files.
+    "no-unused-vars": "off", // disallow declaration of variables that are not used in the code
+    "no-useless-computed-key": "off",
+    "import/no-named-as-default": "off",
+    "import/no-named-as-default-member": "off",
+    "max-len": [
+      2,
+      {
+        code: 80,
+        ignoreComments: true,
+        ignoreUrls: true,
+        ignoreTrailingComments: true,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignorePattern: "^import .*",
+      },
+    ], // specify the maximum length of a line in your program (off by default)
   },
-}
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: false, // Disabling esbuild, since we'll be using swc
+  swc: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    // Additional SWC options can be provided here as needed.
+  },
+  server: {
+    port: 9090, // will be used if available, or the next free one if not
+  },
+  build: {
+    target: 'es2018', // You can adjust the target based on your browser support requirements
+    // Additional build options can be provided here as needed.
+  },
+  // More configuration options can be included based on your project's requirements.
+});
